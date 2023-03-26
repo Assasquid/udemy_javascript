84bb863054248cf670741c914b81de02033c7818
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-03-26 19:51:13 +0100

    Values and variables.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,16 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+console.log(40 + 8 + 23 - 10);
 
-console.log(40 + 8 + 23 - 10); 
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+// Make sure variable names are really descriptive like above, avoid examples below:
+let job1 = 'Programmer';
+let job2 = 'Teacher';
