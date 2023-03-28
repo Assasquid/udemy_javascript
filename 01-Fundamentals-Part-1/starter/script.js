04c5872992ceca5dd4502d949564d1341cc59ae5
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-03-28 18:36:53 +0100

    Complete lecture 13. let, const and var.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,37 @@
+// We declare the age variable:
+let age = 30;
+// We then mutate that variable (reassign a value to that variable):
+age = 31;
+
+// Variables that are not supposed to change at any point, can't change:
+const birthYear = 1988;
+// birthYear = 1990;
+// Will return: "Assignment to a constant variable".
+
+// We can't declare an empty const variable.
+// const firstName;
+// Will return the error "Missing initializer in const declaration".
+
+// Legacy / old way of declaring variables, good to know, to recognise:
+var job = 'Programmer';
+job = 'Teacher';
+// Look similar but below the surface this is different.
+// "let" is "block-scoped" and "var" is "function-scoped".
+// More on this later.
+
+// Some people say we don't need to declare a variable:
+lastName = 'Million-Rousseau'
+console.log(lastName);
+// It would work but it won't create a variable in the current "scope".
+// It creates a property on global object.
+// Bad idea. More on this later.
+
+
+
+
+/* ====================
+CODE FROM 12. DATA TYPES
+
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
@@ -21,6 +55,9 @@ console.log(typeof null);
 // This will return "object" in the console. This is considered a bug.
 // But it's not corrected, for legacy reasons.
 // It should return null.
+
+==================== */
+
 
 /* ====================
 CODE FROM 11. VALUES AND VARIABLES
