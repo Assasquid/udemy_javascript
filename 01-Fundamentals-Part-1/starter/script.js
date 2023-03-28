4ae59f595e1f3384d9167a71ffd9f23101e9f9fb
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-03-28 18:16:58 +0100

    Complete lecture 12. Data Types.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,30 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+// This will return "object" in the console. This is considered a bug.
+// But it's not corrected, for legacy reasons.
+// It should return null.
+
+/* ====================
+CODE FROM 11. VALUES AND VARIABLES
+
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -6,11 +33,11 @@ console.log(23);
 
 let firstName = 'Matilda';
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
 
 let myFirstJob = 'Programmer';
 let myCurrentJob = 'Teacher';
 // Make sure variable names are really descriptive like above, avoid examples below:
 let job1 = 'Programmer';
 let job2 = 'Teacher';
+
+==================== */
