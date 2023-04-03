7ca4be6052efa3c75db22134fc0322d2c9cf31b1
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-04-03 19:07:22 +0100

    Add first else statement.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,6 +3,9 @@ const isOldEnough = age >= 18;
 
 if (isOldEnough) {
     console.log('Sarah can start driving license 🚗 !');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years 😔.`);
 }
 
 
