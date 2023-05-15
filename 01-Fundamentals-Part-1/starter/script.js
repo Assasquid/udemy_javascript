ac6401e4b650050a37af79efb5c77cca329f5308
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-05-15 14:43:07 +0100

    Code for lecture 21.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,25 @@
 
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jerome'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)!");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log('YAY! Height is defined!');
+} else {
+    console.log('Height is UNDEINFED...');
+}
+
+
 /* ====================
 CODE FROM 18. TAKING DECISIONS: IF/ELSE STATEMENTS 
 
