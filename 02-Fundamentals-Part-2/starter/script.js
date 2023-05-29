87beb4a96cb57e9288a1f2ec4e4a7d998a037fc4
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2023-05-29 16:04:01 +0100

    Start section 3: Fundamentals part 2.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// Create a bug on purpose:
+// if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!');
+
+// const interface = 'Audio';
+// const private = 534;
+
+
+
+// ⚠️ SECTION 3: JAVASCRIPT FUNDAMENTALS - PART 2 ⚠️
+
+
+/* ====================
+CODE FROM :
+
+====================
+==================== */
